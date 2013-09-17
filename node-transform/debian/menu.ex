de826f1ea0abd0909c9f81e0c13b36afabf49a18
Matthijs van der Vleuten <git@zr40.nl>
2013-09-17 11:55:12 +0200

    Remove debian packaging example files

--- node-transform/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(python-minerva-node-transform):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="python-minerva-node-transform" command="/usr/bin/python-minerva-node-transform"
