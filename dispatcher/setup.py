da4f6ee279fd3b765ec4a1a129040ea3d690a685
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2013-10-15 16:18:17 +0200

    Add dependency on pyinotify.

--- dispatcher/setup.py
+++ dispatcher/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 setup(name="dispatcher",
     version="2.0.0",
     install_requires=["configobj",
-        "minerva>=3.0.0"],
+        "minerva>=3.0.0", "pyinotify"],
     test_suite="nose.collector",
     package_data={"minerva_dispatcher": ["defaults/*"]},
     packages=["minerva_dispatcher"],
