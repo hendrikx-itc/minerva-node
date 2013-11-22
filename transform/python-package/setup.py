eb2beb7b1aeeb7bffb797b1de649d42e23b5a634
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2013-11-22 11:50:41 +0100

    Update version to match release.

--- transform/python-package/setup.py
+++ transform/python-package/setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name="transforming",
-    version="4.0.0",
+    version="4.4.0",
     author="Hendrikx ITC",
     author_email="info@hendrikx-itc.nl",
     install_requires=["minerva>=3.0"],
