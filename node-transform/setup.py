327a36b050a22c44f06559d60e9ede7c36e91045
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-02-18 11:44:37 +0100

    Bump version for release.

--- node-transform/setup.py
+++ node-transform/setup.py
@@ -16,7 +16,7 @@ __author__ = "Hendrikx ITC"
 
 setup(
     name="transform",
-    version="4.5.5",
+    version="4.5.6",
     description=__doc__,
     author=__author__,
     author_email="a.j.n.blokland@hendrikx-itc.nl",
