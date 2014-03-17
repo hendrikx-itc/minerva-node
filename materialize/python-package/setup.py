076e759ad7d95a9330337dd555f4a980ae2e6e3f
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-03-17 11:51:28 +0100

    Bump version for release.

--- materialize/python-package/setup.py
+++ materialize/python-package/setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name="minerva_materialize",
-    version="4.5.6",
+    version="4.5.7",
     author="Hendrikx ITC",
     author_email="info@hendrikx-itc.nl",
     install_requires=["minerva>=4.5.3"],
