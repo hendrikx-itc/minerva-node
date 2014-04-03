cd357cde8adbf39b4c03f6ec770f2bd395593d19
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-04-03 12:19:48 +0200

    Bump version for release.

--- node-materialize/setup.py
+++ node-materialize/setup.py
@@ -16,7 +16,7 @@ __author__ = "Hendrikx ITC"
 
 setup(
     name="materialize",
-    version="4.5.7",
+    version="4.5.8",
     description=__doc__,
     author=__author__,
     author_email="info@hendrikx-itc.nl",
