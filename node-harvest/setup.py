2f3ee558854376874a8e0711b9feddc0b27091f4
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-08 16:17:38 +0100

    Define dependencies

--- node-harvest/setup.py
+++ node-harvest/setup.py
@@ -21,6 +21,10 @@ setup(
     author=__author__,
     author_email="a.j.n.blokland@hendrikx-itc.nl",
     url = "http://hendrikx-itc.nl",
+    install_requires=[
+        "minerva>=4.6",
+        "minerva-harvesting"
+    ],
     packages=["harvest"],
     package_dir={"": "src"},
     entry_points= {"node.plugins": ["harvest = harvest:HarvestPlugin"]}
