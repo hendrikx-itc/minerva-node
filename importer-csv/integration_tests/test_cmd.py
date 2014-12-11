4898557a4fc5c2725252b1995988a2689b443df9
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2014-12-11 12:30:53 +0100

    Use standard libpsql configuration for integration tests

--- importer-csv/integration_tests/test_cmd.py
+++ importer-csv/integration_tests/test_cmd.py
@@ -1,25 +1,16 @@
-import os
 from subprocess import Popen, PIPE
+from contextlib import closing
 
 from nose.tools import eq_
 
-TEST_CONFIG = """\
-db_uri = {}
-profile_directory = /etc/minerva/csv-importer/profiles/
-log_level = INFO
-log_filename = import-csv.log
-log_directory = /tmp/
-log_rotation_size = 10MB
-""".format(os.getenv("TEST_DB_URL"))
+from minerva.directory.helpers_v4 import DataSource, EntityType
+
+from minerva_db import connect
 
 
 def test_cmd_1():
     file_path = "/tmp/test.csv"
     datasource_name = "test-src"
-    config_file_path = "/tmp/import-csv.conf"
-
-    with open(config_file_path, "wt") as config_file:
-        config_file.write(TEST_CONFIG)
 
     with open(file_path, "wt") as csv_file:
         csv_file.write(
@@ -27,14 +18,23 @@ def test_cmd_1():
             "Node, 001, 12, 232, high\n"
             "Node, 002, 43, 334, medium\n")
 
-    cmd = ["import-csv", "-c", config_file_path, "--timestamp", "now", "--identifier", "Node={name}", datasource_name, file_path]
+    with closing(connect()) as conn:
+        with closing(conn.cursor()) as cursor:
+            DataSource.from_name(cursor, datasource_name)
+            EntityType.from_name(cursor, 'Node')
+
+        conn.commit()
+
+    cmd = [
+        "import-csv", "--timestamp", "now",
+        "--identifier", "Node={name}", "--datasource", datasource_name,
+        file_path
+    ]
     p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
     stdout_data, stderr_data = p.communicate(input=None)
 
-    print(stderr_data)
-
-    #eq_('', stderr_data)
+    eq_('', stderr_data)
 
     eq_('', stdout_data)
 
@@ -42,10 +42,6 @@ def test_cmd_1():
 def test_cmd_2():
     file_path = "/tmp/test.csv"
     datasource_name = "test-src"
-    config_file_path = "/tmp/import-csv.conf"
-
-    with open(config_file_path, "wt") as config_file:
-        config_file.write(TEST_CONFIG)
 
     with open(file_path, "wt") as csv_file:
         csv_file.write(
@@ -53,14 +49,16 @@ def test_cmd_2():
             "Node, 001, 12, 232, high\n"
             "Node, 002, 43, 334, medium\n")
 
-    cmd = ["import-csv", "-c", config_file_path, "--timestamp", "now", "--identifier", "Node={name}", datasource_name, file_path]
+    cmd = [
+        "import-csv", "--timestamp", "now",
+        "--identifier", "Node={name}", "--datasource", datasource_name,
+        file_path
+    ]
     p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
     stdout_data, stderr_data = p.communicate(input=None)
 
-    print(stderr_data)
-
-    #eq_('', stderr_data)
+    eq_('', stderr_data)
 
     eq_('', stdout_data)
 
@@ -68,10 +66,6 @@ def test_cmd_2():
 def test_cmd_identifier_regex():
     file_path = "/tmp/test.csv"
     datasource_name = "test-src"
-    config_file_path = "/tmp/import-csv.conf"
-
-    with open(config_file_path, "wt") as config_file:
-        config_file.write(TEST_CONFIG)
 
     with open(file_path, "wt") as csv_file:
         csv_file.write(
@@ -79,13 +73,41 @@ def test_cmd_identifier_regex():
             "Node, node0001, 12, 232, high\n"
             "Node, node0102, 43, 334, medium\n")
 
-    cmd = ["import-csv", "-c", config_file_path, "--timestamp", "now", "--identifier", "Node={name}", "--identifier-regex", "(Node=)node[0]+(\\d+)", datasource_name, file_path]
+    cmd = [
+        "import-csv", "--timestamp", "now",
+        "--identifier", "Node={name}", "--identifier-regex",
+        "(Node=)node[0]+(\\d+)", "--datasource", datasource_name, file_path
+    ]
+
     p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
     stdout_data, stderr_data = p.communicate(input=None)
 
-    print(stderr_data)
+    eq_('', stderr_data)
 
-    #eq_('', stderr_data)
+    eq_('', stdout_data)
+
+
+def test_cmd_fixed_identifier():
+    datasource_name = "test-src"
+
+    data = (
+        u"a, b, c\n"
+        "12, 232, high\n"
+        "43, 334, medium\n"
+    )
+
+    cmd = [
+        "import-csv", "--timestamp", "now",
+        "--identifier", "Node=001",
+        "--storage-type", "notification",
+        "--datasource", datasource_name
+    ]
+
+    p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+
+    stdout_data, stderr_data = p.communicate(input=data)
+
+    eq_('', stderr_data)
 
     eq_('', stdout_data)
--- importer-csv/integration_tests/test_csvimporter.py
+++ importer-csv/integration_tests/test_csvimporter.py
@@ -10,7 +10,8 @@ the Free Software Foundation; either version 3, or (at your option) any later
 version.  The full license is in the file COPYING, distributed as part of
 this software.
 """
-import StringIO
+from io import StringIO
+from contextlib import closing
 
 from minerva_csvimporter import import_csv
 from minerva_csvimporter.profile import Profile
@@ -19,95 +20,94 @@ from minerva_db import connect
 
 
 def test_timestamp_source_data():
-    conn = connect()
+    with closing(connect()) as conn:
+        readfile = StringIO(
+            u'CIC;CCR;Drops;t_source\n'
+            '10023;0.9919;17;20111111_0000\n'
+            '10047;0.9963;18;20101010_0000\n')
 
-    readfile = StringIO.StringIO(
-        'CIC;CCR;Drops;t_source\n'
-        '10023;0.9919;17;20111111_0000\n'
-        '10047;0.9963;18;20101010_0000\n')
+        profile = Profile({
+            "storage": {
+                "type": "trend",
+                "config": {
+                    "granularity": 86400,
+                    "datasource": "integration_test",
+                    "timestamp_is_start": False
+                }
+            },
+            "identifier": {
+                "template": "Cell={CIC}",
+                "regex": "(.*)"
+            },
+            "timestamp": {
+                "type": "from_column",
+                "config": {
+                    "format": "%Y%m%d_%H%M",
+                    "name": "t_source",
+                    "timezone": "UTC"
+                }
+            },
+            "identifier_is_alias": False,
+            "field_selector": {
+                "type": "all"
+            },
+            "fields": {},
+            "timestamp_is_start": True,
+            "character_encoding": "utf-8",
+            "dialect": {
+                "type": "auto"
+            },
+            "value_mapping": {}
+        })
 
-    profile = Profile({
-        "storage": {
-            "type": "trend",
-            "config": {
-                "granularity": 86400,
-                "datasource": "integration_test",
-                "timestamp_is_start": False
-            }
-        },
-        "identifier": {
-            "template": "Cell={CIC}",
-            "regex": "(.*)"
-        },
-        "timestamp": {
-            "type": "from_column",
-            "config": {
-                "format": "%Y%m%d_%H%M",
-                "name": "t_source",
-                "timezone": "UTC"
-            }
-        },
-        "identifier_is_alias": False,
-        "field_selector": {
-            "type": "all"
-        },
-        "timestamp_is_start": True,
-        "character_encoding": "utf-8",
-        "dialect": {
-            "type": "auto"
-        },
-        "value_mapping": {}
-    })
-
-    import_csv(conn, profile, readfile)
+        import_csv(conn, profile, readfile)
 
 
 def test_timestamp_as_data():
-    conn = connect()
+    with closing(connect()) as conn:
+        readfile = StringIO(
+            u'ts;CIC;CCR;Drops;created\n'
+            '20140511_1300;10023;0.9919;17;20111111_0000\n'
+            '20140511_1300;10047;0.9963;18;20101010_0000\n')
 
-    readfile = StringIO.StringIO(
-        'ts;CIC;CCR;Drops;created\n'
-        '20140511_1300;10023;0.9919;17;20111111_0000\n'
-        '20140511_1300;10047;0.9963;18;20101010_0000\n')
-
-    profile = Profile({
-        "storage": {
-            "type": "trend",
-            "config": {
-                "granularity": 86400,
-                "datasource": "integration_test",
-                "timestamp_is_start": False
-            }
-        },
-        "identifier": {
-            "template": "Cell={CIC}",
-            "regex": "(.*)"
-        },
-        "timestamp": {
-            "type": "from_column",
-            "config": {
-                "format": "%Y%m%d_%H%M",
-                "name": "ts",
-                "timezone": "UTC"
-            }
-        },
-        "identifier_is_alias": False,
-        "field_selector": {
-            "type": "all"
-        },
-        "timestamp_is_start": True,
-        "character_encoding": "utf-8",
-        "dialect": {
-            "type": "auto"
-        },
-        "value_mapping": {
-            "created": {
-                "type": "timestamp",
+        profile = Profile({
+            "storage": {
+                "type": "trend",
                 "config": {
-                    "format": "%Y%m%d_%H%M"
+                    "granularity": 86400,
+                    "datasource": "integration_test",
+                    "timestamp_is_start": False
+                }
+            },
+            "identifier": {
+                "template": "Cell={CIC}",
+                "regex": "(.*)"
+            },
+            "timestamp": {
+                "type": "from_column",
+                "config": {
+                    "format": "%Y%m%d_%H%M",
+                    "name": "ts",
+                    "timezone": "UTC"
+                }
+            },
+            "identifier_is_alias": False,
+            "field_selector": {
+                "type": "all"
+            },
+            "timestamp_is_start": True,
+            "character_encoding": "utf-8",
+            "dialect": {
+                "type": "auto"
+            },
+            "fields": {
+                "created": {
+                    "datatype": "timestamp",
+                    "string_format": {
+                        "format": "%Y%m%d_%H%M"
+                    }
                 }
             }
-        }
-    })
+        })
 
-    import_csv(conn, profile, readfile)
+        import_csv(conn, profile, readfile)
