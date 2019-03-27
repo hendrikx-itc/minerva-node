8a5c8a04e03cd0913cd69c3d092aacf49bcb54f5
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2019-03-27 11:14:12 +0100

    Updated version to 5.0.0.dev1 setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,13 @@ from setuptools import setup
 
 setup(
     name="minerva-node",
-    version="5.0.0",
-    author="Hendrikx ITC",
-    author_email="info@hendrikx-itc.nl",
-    install_requires=["minerva>=5.0", "pika"],
+    author='Hendrikx ITC',
+    author_email='info@hendrikx-itc.nl',
+    version="5.0,0.dev1",
+    install_requires=[
+        "minerva>=5.0.0.dev2",
+        "pika"
+    ],
     test_suite="nose.collector",
     packages=["minerva_node"],
     package_dir={"": "src"},
