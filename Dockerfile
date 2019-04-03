977588288a5edfc14cbbbcf02dab26fb02bd3a5a
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2019-04-03 12:48:57 +0200

    Updated version 5.0.0.dev5

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ MAINTAINER HENDRIKX-ITC
 RUN pip3 install minerva-node
 
 RUN mkdir /etc/minerva -p
-COPY docker-node.yml /etc/minerva/node.yml
+COPY node.yml /etc/minerva/node.yml
 VOLUME /data
 
 CMD ["minerva-node"]
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="minerva-node",
     author='Hendrikx ITC',
     author_email='info@hendrikx-itc.nl',
-     version="5.0.0.dev3",
+    version="5.0,0.dev5",
     install_requires=[
         "minerva-etl>=5.0.0.dev2",
         "pika==0.13.0"
