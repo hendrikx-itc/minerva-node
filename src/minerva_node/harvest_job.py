5f1ff3f35a9a412a03f58e7ec02830498c86e067
Andre Engels <andre.engels@hendrikx-itc.nl>
2020-07-03 10:42:59 +0200

    change minerva-node into a library (MIN-86)

--- src/minerva_node/harvest_job.py
+++ src/minerva_node/harvest_job.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-import os
 import logging
-from functools import partial
-import codecs
-import gzip
-from contextlib import closing
 
-from minerva.directory import DataSource
 from minerva.directory.entitytype import NoSuchEntityType
 from minerva.storage.trend.trendstore import NoSuchTrendStore
-from minerva.storage.trend.datapackage import DataPackage
+from minerva.instance import MinervaInstance
 
 from minerva_node.error import JobError
 from minerva_node.done_actions import execute_action
@@ -23,119 +17,46 @@ class HarvestError(JobError):
 
 
 class HarvestJob:
-    def __init__(self, plugins, conn, description):
-        self.plugins = plugins
+    def __init__(self, parse_method, conn, description):
+        self.parse = parse_method
         self.conn = conn
+        print(description)
+        self.uri = description.get("uri")
+        instance = MinervaInstance.load()
+        self.attribute_stores = {
+            str(attribute_store).lower(): attribute_store
+            for attribute_store in instance.load_attribute_stores()
+        }
+        self.data_source = description.get("data_source")
         self.description = description
-        if 'description' in description:
-            self.description.update(description['description'])
 
     def __str__(self):
-        return "'{}'".format(self.description["uri"])
+        return "'{}'".format(self.uri)
 
     def execute(self):
         try:
-            data_source_name = self.description["data_source"]
+            self.parse(self.conn, self.uri, self.attribute_stores, self.data_source)
 
-            with closing(self.conn.cursor()) as cursor:
-                data_source = DataSource.get_by_name(data_source_name)(cursor)
-
-                if data_source is None:
-                    raise HarvestError(
-                        "no data source with name '{}'".format(data_source_name)
-                    )
-
-            parser_config = self.description.get("parser_config", {})
-            uri = self.description["uri"]
-
-            data_type = self.description["data_type"]
-        except Exception as err:
-            err.args += (str(self.description),)
-            raise
-
-        try:
-            plugin = self.plugins[data_type]
-        except KeyError:
-            raise HarvestError(
-                "could not load parser plugin '{}' - not in {}".format(
-                    data_type, ', '.join(self.plugins.keys())
-                )
-            )
-
-        parser = plugin.create_parser(parser_config)
-
-        encoding = self.description.get("encoding", "utf-8")
-
-        try:
-            data_stream = open_uri(uri, encoding)
+        except NoSuchTrendStore as exc:
+            # This can happen normally, when no trend store is
+            # configured for the data in the package, but you might
+            # want to see what data you are missing using debug
+            # logging.
+            self.conn.rollback()
+            logging.debug(str(exc))
+        except NoSuchEntityType as exc:
+            # For a similar reason as the above exception, no entity
+            # type might be found.
+            self.conn.rollback()
+            logging.debug(str(exc))
         except Exception as exc:
-            raise JobError(str(exc))
-
-        logging.debug("Opened '{}'".format(uri))
-
-        action = {
-            'type': 'harvest',
-            'plugin': data_type,
-            'uri': uri
-        }
-
-        store_commands = (
-            parser.store_command()(package, action)
-            for package in DataPackage.merge_packages(
-                parser.load_packages(
-                    data_stream, os.path.basename(uri)
-                )
-            )
-        )
-
-        try:
-            for store_cmd in store_commands:
-                try:
-                    store_cmd(data_source)(self.conn)
-                except NoSuchTrendStore as exc:
-                    # This can happen normally, when no trend store is
-                    # configured for the data in the package, but you might
-                    # want to see what data you are missing using debug
-                    # logging.
-                    self.conn.rollback()
-                    logging.debug(str(exc))
-                except NoSuchEntityType as exc:
-                    # For a similar reason as the above exception, no entity
-                    # type might be found.
-                    self.conn.rollback()
-                    logging.debug(str(exc))
-
-        except Exception as exc:
-            logging.error("Failure executing job '{}': {}".format(uri, str(exc)))
-
+            logging.error("Failure executing job '{}': {}".format(self.uri, str(exc)))
             execute_action(
-                uri, self.description.get("on_failure", DEFAULT_ACTION)
+                self.uri, self.description.get("on_failure", DEFAULT_ACTION)
             )
-
-            raise JobError(str(exc))
         else:
             execute_action(
-                uri, self.description.get("on_success", DEFAULT_ACTION)
+                self.uri, self.description.get("on_success", DEFAULT_ACTION)
             )
 
-            logging.debug("Finished job '{}'".format(uri))
-
-
-def open_uri(uri, encoding):
-    """
-    Return a file object for the specified URI.
-    """
-    if uri.endswith(".gz"):
-        open_action = partial(gzip.open, uri)
-    else:
-        if encoding == "binary":
-            open_action = partial(open, uri, "rb")
-        else:
-            open_action = partial(codecs.open, uri, encoding=encoding)
-
-    try:
-        return open_action()
-    except IOError as exc:
-        raise HarvestError("Could not open {0}: {1!s}".format(uri, exc))
-    except LookupError as exc:
-        raise HarvestError(str(exc))
+            logging.debug("Finished job '{}'".format(self.uri))
--- src/minerva_node/node.py
+++ src/minerva_node/node.py
@@ -3,21 +3,67 @@ import logging
 import json
 import traceback
 
-from minerva.harvest.plugins import load_plugins
+import psycopg2
+import yaml
+import signal
+import threading
+from time import sleep
+from operator import not_
+
+from minerva.util import compose, retry_while
+from minerva.db import connect
 
 from minerva_node.pika_consumer import Consumer
 from minerva_node.harvest_job import HarvestJob
 from minerva_node.error import JobError, JobDescriptionError
+from minerva_node.log import level_map
 
 
 class Node(Consumer):
-    def __init__(self, conn, config):
+    SIGNAL_MAP = {
+        signal.SIGHUP: "SIGHUP",
+        signal.SIGKILL: "SIGKILL",
+        signal.SIGTERM: "SIGTERM",
+        signal.SIGINT: "SIGINT",
+        signal.SIGUSR1: "SIGUSR1"
+        }
+
+    def __init__(self, parser, url, queue, key="minerva", logger=None, log_level="WARNING"):
+
         Consumer.__init__(
-            self, config['url'], config['queue'], config['logger']
+            self, url, queue, key, logger
         )
-        self.config = config
-        self.conn = conn
-        self.plugins = load_plugins()
+        self.parser = parser
+        log_level = level_map.get(log_level)
+        self.setup_logging(log_level)
+        self.stop_event = threading.Event()
+
+    def start_node(self):
+        signal.signal(signal.SIGTERM, self.stop_node)
+        signal.signal(signal.SIGINT, self.stop_node)
+        signal.signal(signal.SIGHUP,self.stop_node)
+
+        handler_map = {
+            psycopg2.OperationalError: lambda exc: logging.error(
+                "could not connect to database ({}), waiting".format(exc)
+            )
+        }
+
+        retry_condition = compose(not_, self.stop_event.is_set)
+
+        self.dbconnection = retry_while(
+            connect, handler_map, retry_condition
+        )
+
+        if self.dbconnection:
+            logging.info("Starting consumer")
+
+            self.run()
+
+            while self.is_alive() and not self.stop_event.is_set():
+                sleep(1)
+
+        logging.info("Stopped")
 
     def on_reception(self, body):
         job = self.create_job(body)
@@ -52,9 +98,34 @@ class Node(Consumer):
         """
         try:
             job_description = json.loads(job_description)
+            if 'description' in job_description.keys():
+                job_description.update(job_description['description'])
         except ValueError as e:
             raise JobDescriptionError("Invalid job description") from e
 
-        return HarvestJob(
-            self.plugins, self.conn, job_description
+        job = HarvestJob(
+            self.parser, self.dbconnection, job_description
         )
+
+        return job
+
+    def load_config(self, file_path):
+        with open(file_path) as config_file:
+            return yaml.load(config_file, Loader=yaml.SafeLoader)
+
+    def setup_logging(self, log_level):
+        log_handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(levelname)s %(message)s")
+        log_handler.setFormatter(formatter)
+
+        root_logger = logging.getLogger("")
+        root_logger.setLevel(log_level)
+        root_logger.addHandler(log_handler)
+
+    def stop_node(self, signum, _frame):
+        logging.info(
+            "received {0!s} signal".format(self.SIGNAL_MAP.get(signum, signum))
+        )
+
+        self.stop_event.set()
+        self.stop()
