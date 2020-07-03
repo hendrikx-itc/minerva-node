0d6bc2ba37a108090959306424bf861630520d05
Andre Engels <andre.engels@hendrikx-itc.nl>
2020-07-03 10:47:55 +0200

    Revert "new version number"
    This reverts commit 7b1fc55e7d0fa5158b78467052a3ee1fa549c1b1.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="minerva-node",
     author='Hendrikx ITC',
     author_email='info@hendrikx-itc.nl',
-    version="v6.0.0",
+    version="v5.0.3",
     install_requires=[
         "minerva-etl>=5.1.0",
         "pika==0.13.0",
